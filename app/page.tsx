c2bf1c293c4e009dae45618f4cfbf7264971f60b
Anant Rawat <anant1995rawat1995@gmail.com>
2024-10-18 15:56:00 +0530

    feat : Implement timer and clx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
+"use client";
+
 import Image from "next/image";
 import { assets } from "@/utils/assets-utils";
+import { type Framework, frameworks } from "@/utils/framework-utils";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [currentFramework, setCurrentFramework] = useState<Framework>(
+    frameworks[0]
+  );
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const rotateFrameworks = () => {
+      setCurrentFramework(frameworks[currentIndex]);
+      currentIndex = (currentIndex + 1) % frameworks.length;
+    };
+
+    const intervalId = setInterval(rotateFrameworks, 2000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
   return (
     <main>
+      <div className="fixed inset-0 transition-color delay-100 duration-700 opacity-25" />
       <Image
         alt="gradient"
         width={1200}
